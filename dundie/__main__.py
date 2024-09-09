a973a7460553d5e56f014b270f9f257df56ca825
Matheus Santos <matheus.snts.pvh@gmail.com>
2024-09-09 18:48:20 -0400

    Setup up project initial organization and metadata

---
+++ dundie/__main__.py
@@ -0,0 +1,2 @@
+def main():
+    print("executing entry point dundie")
---
+++ setup.py
@@ -0,0 +1,10 @@
+#setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name = "dundie",
+    version= "0.1.0",
+    description="Rewards Point System for Dunder Mifflin",
+    author="Matheus",
+    packages=find_packages(),
+)
