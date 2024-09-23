88a2671083e6bf436d3d3bfecd80cb076aab7c96
Matheus Santos <matheus.snts.pvh@gmail.com>
2024-09-23 18:09:34 -0400

    "Add peaple.csv"

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,2 +1,37 @@
+import argparse
+
+def load(filepath):
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError:
+        print("Filepath is invalid")
+
+
 def main():
-    print("executing entry point dundie")
+    parse = argparse.ArgumentParser(
+    description="Dunder Mifflin Rewards CLI",
+    epilog="Enjoy and use with cautious.", 
+    )
+    parse.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load","show","send"),
+        default="help",
+        
+    )
+    parse.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None
+    )
+    args = parse.parse_args()
+  
+    globals()[args.subcommand](args.filepath)
+   
+    
+if __name__== "__main__":
+    main()
